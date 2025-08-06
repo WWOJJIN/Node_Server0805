2358890c792fd34a00104600afc5b695e62b1d5c
Woojin <119497359+WWOJJIN@users.noreply.github.com>
2025-08-06 11:19:32 +0900

    캐릭터완

--- index.js
+++ index.js
@@ -1,24 +1,16 @@
 const express = require("express")
 const app = express()
 const PORT = 3000
-
-
-
+const characterRouter = require('./routes/character')
 
 app.use(express.json())
-
-const boardRouter = require('./routes/board')
-app.use('/board', boardRouter)
-
-
-
+app.use('/char', characterRouter)
 
 
 
 app.get("/", (req, res) => {
-    res.send("Hello woojin!")
-});
-
+    res.send("Hello Express!")
+})
 app.listen(PORT, () => {
     console.log("Server is running!")
-});
+})
